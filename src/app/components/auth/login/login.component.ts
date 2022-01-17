37b9d05fad607ac7468556bafc74f92cb10d4e3b
Damian <damianrossi@live.com.ar>
2022-01-17 10:35:00 +0100

    minor fix

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {NgForm} from '@angular/forms';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
